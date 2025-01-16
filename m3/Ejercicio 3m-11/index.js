9e2273a89380777aa889c15a9f6c75705aa22b36
Guasmo <juancalveti4@gmail.com>
2025-01-16 14:38:03 -0500

    Update index.js

--- m3/Ejercicio 3m-11/index.js
+++ m3/Ejercicio 3m-11/index.js
@@ -2,7 +2,7 @@
 let nums = [2, 3, 2, 3, 5];
 let outPut = 'Salida:\n';
 
-for (let i = 0; i < 4; i++) {  // cambiado de nums.length a 4
-    outPut += i + ' ' + (nums.length - 1 - i) + '\n';
+for (let i = 0; i < nums.length - 1; i++) {  // usamos nums.length - 1 para iterar 4 veces
+    outPut += i + ' ' + (nums.length - 2 - i) + '\n';  // nums.length - 2 - i para la salida invertida
 }
 alert(outPut);
